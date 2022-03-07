451e9f9e1f3173e7eb0c103a22b84937509f1fe0
griegner <johndoe@example.com>
2022-03-07 08:36:01 -0800

    format code

--- plot_live.py
+++ plot_live.py
@@ -2,23 +2,23 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
 fig = plt.figure()
-open('plot.txt', 'w').close()  # clear file
+open("plot.txt", "w").close()  # clear file
 
 
 def animate(i):
-    data = open('plot.txt', 'r').read()
-    data_list = data.split('\n')
+    data = open("plot.txt", "r").read()
+    data_list = data.split("\n")
     xs = []
     ys = []
     for line in data_list:
         if len(line) > 0:
-            x, y = line.split(' ')
+            x, y = line.split(" ")
             xs.append(float(x))
-            ys.append(float(y)/10)
+            ys.append(float(y) / 10)
     plt.cla()
-    plt.plot(xs, ys, 'k', lw=.5)
-    plt.xlabel('time (sec)')
-    plt.ylabel('temperature')
+    plt.plot(xs, ys, "k", lw=0.5)
+    plt.xlabel("time (sec)")
+    plt.ylabel("temperature")
 
 
 def main():
--- run_tcs.py
+++ run_tcs.py
@@ -5,84 +5,87 @@ from time import sleep
 
 # `$ python -m serial.tools.list_ports`
 
+
 def open_port():
     # set port
-    tcs_port = '/dev/cu.usbmodem14401'
+    tcs_port = "/dev/cu.usbmodem14401"
     tcs = serial.Serial(tcs_port, baudrate=115200, timeout=1)
-    sleep(.001)
-    tcs.write(b'F')
+    sleep(0.001)
+    tcs.write(b"F")
     return tcs
 
 
 def port_status(tcs):
     if tcs.is_open:
-        sleep(.001)
-        tcs.write(b'B')
+        sleep(0.001)
+        tcs.write(b"B")
         battery = tcs.read_until().decode()
         if len(battery) > 0:
-            print('\ntcs port open:', datetime.now().strftime('%H:%M %m/%d/%y'))
+            print("\ntcs port open:", datetime.now().strftime("%H:%M %m/%d/%y"))
             print(battery)
         else:
-            print('\nturn tcs power ON')
+            print("\nturn tcs power ON")
             raise KeyboardInterrupt
     else:
-        print('\ntcs port closed:', datetime.now().strftime('%H:%M %m/%d/%y'))
+        print("\ntcs port closed:", datetime.now().strftime("%H:%M %m/%d/%y"))
 
 
 def set_parameters(tcs, parameters):
     for i in parameters:
-        sleep(.001)
+        sleep(0.001)
         tcs.write(i.encode())
 
 
 def replace(var, st):
     for i in st:
-        var = var.replace(i, '')
+        var = var.replace(i, "")
     return var
 
 
 def print_parameters(tcs):
-    sleep(.001)
-    tcs.write(b'F')
-    sleep(.001)
-    tcs.write(b'P')
-    par = tcs.read_until().decode().replace('\r', '')
-    print('\nparameters: ')
-    print('global:', par[:28])
-    print('zone 1:', par[28:51])
-    print('zone 2:', par[51:74])
-    print('zone 3:', par[74:97])
-    print('zone 4:', par[97:120])
-    print('zone 5:', par[120:], '\n')
+    sleep(0.001)
+    tcs.write(b"F")
+    sleep(0.001)
+    tcs.write(b"P")
+    par = tcs.read_until().decode().replace("\r", "")
+    print("\nparameters: ")
+    print("global:", par[:28])
+    print("zone 1:", par[28:51])
+    print("zone 2:", par[51:74])
+    print("zone 3:", par[74:97])
+    print("zone 4:", par[97:120])
+    print("zone 5:", par[120:], "\n")
 
 
 def start_stim(tcs):
-    sleep(.001)
-    tcs.write(b'O')
+    sleep(0.001)
+    tcs.write(b"O")
     tcs.reset_output_buffer()
     tcs.reset_input_buffer()
-    sleep(.001)
-    tcs.write(b'L')
+    sleep(0.001)
+    tcs.write(b"L")
 
 
 def stim(tcs, start):
 
     # plot.txt is read by plot_live.py
-    open('plot.txt', 'w').close()  # clear file
+    open("plot.txt", "w").close()  # clear file
 
     start_stim(tcs)
 
     # the tcs will return the current temperatures of the five zones at 100Hz
-    for _ in range(1000):    # range dependent on parameter Y9999, writes 9.99sec of temp data
+    for _ in range(
+        1000
+    ):  # range dependent on parameter Y9999, writes 9.99sec of temp data
         temp = tcs.read_until().decode().strip()
-        temp = replace(temp, '+.')
+        temp = replace(temp, "+.")
 
         x = time.time() - start
-        y = temp[0:3]  # select zone 1 for plotting
+        y = temp[:3]
 
         if len(temp) > 1:
-            with open('plot.txt', 'a') as file_out:
-                print('%s %s' % (x, y), file=file_out)
+            with open("plot.txt", "a") as file_out:
+                print("%s %s" % (x, y), file=file_out)
 
 
 def main():
@@ -93,24 +96,24 @@ def main():
         port_status(tcs)
 
         # define stim parameters
-        set_parameters(tcs, parameters='N350 C0480 S11111 D006000 Y9999')
+        set_parameters(tcs, parameters="N350 C0480 S11111 D006000 Y9999")
         print_parameters(tcs)
 
-        input('[enter]: ')
+        input("[enter]: ")
 
         # start stim, run 10x
         start = time.time()
         for counter in range(1, 11):
 
-            print('heat%d' % counter)
+            print("heat%d" % counter)
             stim(tcs, start)
 
-            print('neutral%d' % counter)
+            print("neutral%d" % counter)
             sleep(6)
 
     except KeyboardInterrupt:
-        sleep(.001)
-        tcs.write(b'A')
+        sleep(0.001)
+        tcs.write(b"A")
 
     tcs.close()
     port_status(tcs)
